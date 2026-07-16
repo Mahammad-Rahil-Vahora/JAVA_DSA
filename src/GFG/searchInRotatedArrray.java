1af57f0b338485968a8da98d17b847f141e7af85
Mahammad-Rahil-Vahora <rahilvahora7430@gamil.com>
2026-07-16 07:26:38 +0530

    Add New problems and Solutions

---
+++ src/GFG/searchInRotatedArrray.java
@@ -0,0 +1,35 @@
+package GFG;
+
+public class searchInRotatedArrray {
+    static void main(String[] args) {
+        int [] arr = {4,5,6,7,0,1,2};
+        int target = 2;
+
+        int ans = search(arr,target);
+        System.out.println(ans);
+    }
+
+    public static int search (int [] arr, int target) {
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+
+        while (low <= high){
+            int mid = low + (high-low)/2;
+
+            if (arr[mid] == target) return mid;
+
+            if(arr[low] < arr[mid]){
+                if(arr[low] <= target && target < arr[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+            else {
+                if(arr[mid] > target && target <= arr[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+}
