0093bf16ee5dadbb7a7e1d44f2cb7a04a2b4896b
Mahammad-Rahil-Vahora <rahilvahora7430@gamil.com>
2026-07-21 22:28:21 +0530

    Add New Problems And Solution

--- src/Binary_Search/searchInsertPosition.java
+++ src/Binary_Search/searchInsertPosition.java
@@ -3,7 +3,7 @@ package Binary_Search;
 public class searchInsertPosition {
     static void main(String[] args) {
         int [] arr = {1,3,5,6};
-        int target = 7;
+        int target = 0;
         int ans = insertPosition(arr,target);
         System.out.println(ans);
     }
--- src/Binary_Search/validPerfectSqure.java
+++ src/Binary_Search/validPerfectSqure.java
@@ -11,13 +11,13 @@ public class validPerfectSqure {
 
         int low = 0;
         int high = n - 1;
-
+        if(n == 0 || n == 1) return true;
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if (mid * mid == n) {
+            if (mid == n / mid && n % mid == 0) {
                return true;
-            } else if (mid * mid < n) {
+            } else if (mid < n / mid) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
