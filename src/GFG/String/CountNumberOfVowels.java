bbc7fcd5c04c359c6f1653be0904e82afea82e45
Mahammad-Rahil-Vahora <rahilvahora7430@gamil.com>
2026-08-04 18:25:18 +0530

    Add New Problems And Solutions

---
+++ src/GFG/String/CountNumberOfVowels.java
@@ -0,0 +1,13 @@
+package GFG.String;
+
+public class CountNumberOfVowels {
+    static void main(String[] args) {
+        String s = "Mahammad_Rahil";
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+           char ch = s.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ) count++;
+        }
+        System.out.println(count);
+    }
+}
