b3aeb9049b2db8245b031a6a27103cf277f779a9
Mahammad-Rahil-Vahora <rahilvahora7430@gamil.com>
2026-08-07 19:36:28 +0530

    Update the file

--- src/Array/Level_4/maximumProductSubArray.java
+++ src/Array/Level_4/maximumProductSubArray.java
@@ -2,8 +2,19 @@ package Array.Level_4;
 
 public class maximumProductSubArray {
     static void main(String[] args) {
-        int [] arr = {2,3,-2,4};
-        int n = arr.length;
+        int [] nums = {2,3,-2,4};
 //        op : 6    2,3
+        int n = nums.length;
+        int ans = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+
+            for (int j = i; j < n; j++) {
+                product *= nums[j];
+                ans = Math.max(ans, product);
+            }
+        }
+        System.out.println(ans);
     }
 }
